06fbce22971406c3c001288497070310f5a95130
adamchengtkc <kiucheng@gmail.com>
2020-05-11 09:16:24 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     author="Adam Cheng",
     author_email="tkc@equinor.com",
     description="Unofficial PetroMod API",
-    long_description="Unofficial Python API for reading and writing PetroMod models,
+    long_description="Unofficial Python API for reading and writing PetroMod models",
     long_description_content_type="text/markdown",
     url="https://github.com/equinor/pyPMod",
     packages=setuptools.find_packages(),
