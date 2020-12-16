32089a51793145f6e2902ee6976ed5edbe405435
Adam Cheng <TKC@equinor.com>
2020-12-16 11:40:24 +0100

    update from subheats modification

--- petromodder/petromod.py
+++ petromodder/petromod.py
@@ -1,12 +1,14 @@
 import pandas as pd
+import xtgeo
 from pathlib import Path
 import os
 import xml.etree.ElementTree as ET
 import warnings
 import re
-from shapely.geometry import Point, Polygon
+from shapely.geometry import Point, Polygon,LineString
 import numpy as np
 import datetime
+import xtgeo
 
 tested_version = ["2017.1", "2019.1"]
 
@@ -17,6 +19,7 @@ class PetroMod:
         self.facies_maps = self.path / "in//ts1//bl1//faci_m//cont.pmt"
         self.depth_maps = self.path / "in//ts1//bl1//dpth_m//cont.pmt"
         self.heatflow_maps = self.path / "in//hf_m//cont.pmt"
+        self.pwd_maps = self.path / "in//pdp_m//cont.pmt"
         self.maps = self.path / "in//ts1//bl1//"  # for subsheat building
 
     @property
@@ -311,6 +314,18 @@ class PetroMod:
         return version
 
     @property
+    def grid_size(self):
+        aoi_table = self.read_pmt(self.path / "in//aoi_m//cont.pmt")
+        aoi_file_name = aoi_table.ID[0]
+        aoi_file_name = str(aoi_file_name)
+        aoi_file_name = aoi_file_name + ".pmd"
+        aoi_map_path = self.path / "in//aoi_m//" / aoi_file_name
+        aoi = xtgeo.surface_from_file(aoi_map_path, fformat="petromod")
+        if aoi.xinc != aoi.yinc:
+            warnings.warn("Different x and y intervals is not supported")
+        return {"xinc": aoi.xinc, "yinc": aoi.yinc}
+
+    @property
     def heatflow_age(self):
         try:
             df = self.read_pmt(self.path / "in//heat.pmt")
@@ -318,6 +333,203 @@ class PetroMod:
             df = df.astype(int)
         except:
             raise Exception("No heatflow table in PM. Open PM and create table first")
+        return df
+
+    def add_heatflow_maps(self, map_dict, folder_name=None):
+        """[summary]
+
+        Args:
+            map_dict (dict): Dictionary containing: Keys: Age of map in string. Value: HF map in xtgeo surface object
+            folder_name (str, optional): Name of folder of maps in PetroMod. Defaults to None.
+        """
+        # Check existing HF map index
+        try:
+            df = self.heatflow_maps_index
+            max_id = df["ID"].max() + 1
+        except:  # create new
+            if os.path.isdir(self.path / "in//hf_m") == False:
+                warnings.warn(
+                    "Heat flow maps folder does not exist. Creating an empty folder..."
+                )
+                os.mkdir(self.path / "in//hf_m")
+            else:
+                warnings.warm("No heatflow map index file found. Creating cont.pmt..")
+            df = pd.DataFrame(columns=["Key", "ID", "Level", "NameInTree"])
+            max_id = 1
+        # Add new folder to the index
+        if isinstance(folder_name, str):
+            df = df.append(
+                {"Key": "Data", "ID": max_id, "Level": 1, "NameInTree": folder_name},
+                ignore_index=True,
+            )
+            max_id = max_id + 1
+        elif isinstance(folder_name, type(None)):
+            pass
+        else:
+            pass
+        # Export HF map as PMD and update hf map index
+        for k, v in map_dict.items():
+            f = "in//hf_m/" + str(max_id) + ".pmd"
+            filepath = self.path / f
+            filepath = str(filepath.resolve())
+            v.to_file(filepath, fformat="petromod", pmd_dataunits=(15, 44))
+            map_dict.update({k: max_id})
+            # construct HF maps index table
+            nameintree = k + "Ma"
+            df = df.append(
+                {"Key": "Data", "ID": max_id, "Level": 2, "NameInTree": nameintree},
+                ignore_index=True,
+            )
+            max_id = max_id + 1
+        headers = [
+            ["Head", "ID", "Level", "Name"],
+            ["Key", "ID", "Level", "NameInTree"],
+            ["Format", "%4d", "%2d", "%s"],
+        ]
+        # update hf map index
+        output = self.write_pmt("Standard Content", df, 4, headers)
+        with open(self.path / "in//hf_m//cont.pmt", "w+") as f:
+            f.write(output)
+        return map_dict
+
+    def update_heatflow_model(self, map_dict):
+        age = np.empty(0)
+        maps_file_id = np.empty(0)
+        for k, v in map_dict.items():
+            a = float(k)
+            age = np.append(age, a)
+            maps_file_id = np.append(maps_file_id, v)
+        size = age.size
+        key = np.full(size, "Data")
+        mode = np.ones(size)
+        value = np.full(size, 99999.0000)  # 99999.0000 in PM means use map for HF
+        data = {
+            "Key": key,
+            "Age": age,
+            "Mode": mode,
+            "Value": value,
+            "Map": maps_file_id,
+        }
+        df = pd.DataFrame.from_dict(data)
+
+        headers = [
+            ["Head", "Age from", "Mode", "Value", "Map"],
+            ["Head", "[Ma]", "", "[mW/m^2]", ""],
+            ["Key", "Age", "Mode", "Value", "Map"],
+            ["Format", "%6.4f", "%d", "%6.4f", "%d"],
+        ]
+        output = self.write_pmt("Heat Flow", df, 5, headers)
+        with open(self.path / "in//heat.pmt", "w+") as f:
+            f.write(output)
+        return
+
+    def add_paleo_water_depth_maps(
+        self, map_dict, folder_name=None, update_model=False
+    ):
+        """[summary]
+
+        Args:
+            map_dict (dict): Dictionary containing: Keys: Age of map in string. Value: PWD map in xtgeo surface object
+            folder_name (str, optional): Name of folder of maps in PetroMod. Defaults to None.
+            update_model (bool, optional): Set PWD maps to active in PetroMod. Defaults to False.
+        """
+        # Check existing HF map index
+        try:
+            df = self.pwd_maps_index
+            max_id = df["ID"].max() + 1
+        except:  # create new
+            if os.path.isdir(self.path / "in//pdp_m") == False:
+                warnings.warn(
+                    "Paleo Water Depth maps folder does not exist. Creating an empty folder..."
+                )
+                os.mkdir(self.path / "in//pdp_m")
+            else:
+                warnings.warm(
+                    "No Paleo Water Depth map index file found. Creating cont.pmt.."
+                )
+            df = pd.DataFrame(columns=["Key", "ID", "Level", "NameInTree"])
+            max_id = 1
+        # Add new folder to the index
+        if isinstance(folder_name, str):
+            df = df.append(
+                {"Key": "Data", "ID": max_id, "Level": 1, "NameInTree": folder_name},
+                ignore_index=True,
+            )
+            max_id = max_id + 1
+        elif isinstance(folder_name, type(None)):
+            pass
+        else:
+            pass
+        # Export HF map as PMD and update hf map index
+        for k, v in map_dict.items():
+            f = "in//pdp_m/" + str(max_id) + ".pmd"
+            filepath = self.path / f
+            filepath = str(filepath.resolve())
+            v.to_file(filepath, fformat="petromod", pmd_dataunits=(15, 10))
+            map_dict.update({k: max_id})
+            # construct HF maps index table
+            nameintree = k + "Ma"
+            df = df.append(
+                {"Key": "Data", "ID": max_id, "Level": 2, "NameInTree": nameintree},
+                ignore_index=True,
+            )
+            max_id = max_id + 1
+        headers = [
+            ["Head", "ID", "Level", "Name"],
+            ["Key", "ID", "Level", "NameInTree"],
+            ["Format", "%4d", "%2d", "%s"],
+        ]
+        # update hf map index
+        output = self.write_pmt("Standard Content", df, 4, headers)
+        with open(self.path / "in//pdp_m//cont.pmt", "w+") as f:
+            f.write(output)
+        return map_dict
+
+    def update_paleo_water_depth_model(self, map_dict):
+        age = np.empty(0)
+        maps_file_id = np.empty(0)
+        for k, v in map_dict.items():
+            a = float(k)
+            age = np.append(age, a)
+            maps_file_id = np.append(maps_file_id, v)
+        size = age.size
+        key = np.full(size, "Data")
+        mode = np.ones(size)
+        ref = np.zeros(size)
+        layer = np.ones(size) * -1
+        value = np.full(size, 99999.0000)  # 99999.0000 in PM means use map for HF
+        data = {
+            "Key": key,
+            "AgeFrom": age,
+            "Reference": ref,
+            "Layer": layer,
+            "Mode": mode,
+            "Value": value,
+            "Map": maps_file_id,
+        }
+        df = pd.DataFrame.from_dict(data)
+
+        headers = [
+            ["Head", "Age from", "Reference", "Layer", "Mode", "Depth", "Depth Map",],
+            ["Head", "[Ma]", "", "", "", "[m]", ""],
+            ["Key", "AgeFrom", "Reference", "Layer", "Mode", "Value", "Map"],
+            ["Format", "%6.4f", "%d", "%d", "%d", "%6.4f", "%d"],
+        ]
+        output = self.write_pmt("Water Depth / Paleo Depth", df, 7, headers)
+        with open(self.path / "in//palg.pmt", "w+") as f:
+            f.write(output)
+        return
+
+    @property
+    def pwd_age(self):
+        try:
+            df = self.read_pmt(self.path / "in//palg.pmt")
+            df = df["AgeFrom"].values
+            df = df.astype(int)
+        except:
+            raise Exception(
+                "No Paleo-water depth table in PM. Open PM and create table first"
+            )
         return df
 
     @property
@@ -336,6 +548,16 @@ class PetroMod:
                 df = df.drop(df.columns[-1], axis=1)
         except:
             raise Exception("No heatflow maps in PM")
+        return df
+
+    @property
+    def pwd_maps_index(self):
+        try:
+            df = self.read_pmt(self.path / "in//pdp_m/cont.pmt")
+            while df.shape[1] > 4:
+                df = df.drop(df.columns[-1], axis=1)
+        except:
+            raise Exception("No Paleo-water depth maps in PM")
         return df
 
     @property
@@ -393,6 +615,7 @@ class PetroMod:
             dict -- key: polygon name, value: Shapely object
         """
         poly_all = {}
+        line_all={}
         filepath = self.path.parents[1] / "cult"
         fpath = filepath / item
         df = self.read_pmt(fpath)
@@ -400,14 +623,29 @@ class PetroMod:
         poly_id = pd.unique(df["GID"]).astype(int)
         for ind, i in enumerate(poly_id):
             df_filtered = df[df["GID"] == i]
-            xx = df_filtered["X"]
-            yy = df_filtered["Y"]
-            ls_pt = np.dstack((xx, yy))[0]
-            Point_ls_pt = []
-            for i in ls_pt:
-                x = Point(i)
-                Point_ls_pt.append(x)
-            coords = [p.coords[:][0] for p in Point_ls_pt]
-            poly = Polygon(coords)
-            poly_all.update({poly_name[ind]: poly})
-        return poly_all
+            first = df_filtered.iloc[0]
+            last = df_filtered.iloc[-1]
+            if first.equals(last) == True:
+                xx = df_filtered["X"]
+                yy = df_filtered["Y"]
+                ls_pt = np.dstack((xx, yy))[0]
+                Point_ls_pt = []
+                for i in ls_pt:
+                    x = Point(i)
+                    Point_ls_pt.append(x)
+                coords = [p.coords[:][0] for p in Point_ls_pt]
+                poly = Polygon(coords)
+                poly_all.update({poly_name[ind]: poly})
+            else:
+                xx = df_filtered["X"]
+                yy = df_filtered["Y"]
+                ls_pt = np.dstack((xx, yy))[0]
+                Point_ls_pt = []
+                for i in ls_pt:
+                    x = Point(i)
+                    Point_ls_pt.append(x)
+                coords = [p.coords[:][0] for p in Point_ls_pt]
+                poly = LineString(coords)
+                line_all.update({poly_name[ind]: poly})
+        line_poly = {"polygon":poly_all, "lines":line_all}
+        return line_poly
