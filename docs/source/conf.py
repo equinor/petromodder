132cb02f751fb0720250326ba92405563c30c89a
Adam Cheng <52572642+adamchengtkc@users.noreply.github.com>
2021-10-18 12:44:34 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,9 @@
 import os
 import sys
 from datetime import date
-import petromodder
 sys.path.insert(0, os.path.abspath("."))
+import petromodder
+
 
 
 # -- Project information -----------------------------------------------------
