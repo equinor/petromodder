a8bc651c98c4dbb52b426f962f537947f82cf725
Adam Cheng <52572642+adamchengtkc@users.noreply.github.com>
2021-10-19 11:45:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,5 +35,5 @@ setuptools.setup(
         "xtgeo>=2.14.1",
     ],
     python_requires=">=3.7,<=3.9.7",
-    setup_requires=['better-setuptools-git-version'],
+    setup_requires=["setuptools-git-versioning"],
 )
