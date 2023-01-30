aa6218d2559aba8ce27869bee8d66fe25be23677
Adam Cheng <52572642+adamchengtkc@users.noreply.github.com>
2023-01-30 09:50:02 +0000

    python 3.10 support

--- setup.py
+++ setup.py
@@ -34,6 +34,6 @@ setuptools.setup(
         "tabulate>=0.8.3",
         "xtgeo>=2.14.1",
     ],
-    python_requires=">=3.7,<3.10",
+    python_requires=">=3.7,<3.11",
     setup_requires=["setuptools-git-versioning"],
 )
